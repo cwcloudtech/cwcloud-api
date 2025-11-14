01a01088ed235c3017f78430e37d5bcf43e606fc
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2025-11-14 10:20:46 +0000

    Release: ce-3.17.9-7833243a

--- src/schemas/Payment.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class PaymentSchema(BaseModel):
-    invoice_id: str
-    voucher_id: Optional[str] = None
-
-class PaymentRelaunchSchema(BaseModel):
-    invoice_id: str
